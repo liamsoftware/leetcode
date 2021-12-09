d6017e5f4645e7415ea1f03166f4cc992049f0f1
Liam Hayes <liamhayes@Ganondorf.local>
2021-12-09 10:20:15 +0000

    1528 Shuffle String [Easy]

---
+++ src/com/liamhayes/ShuffleString.java
@@ -0,0 +1,24 @@
+package com.liamhayes;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        System.out.println(restoreString(s, indices));
+    }
+    public static String restoreString(String s, int[] indices) {
+        String newS = "";
+        boolean found = false;
+
+        for (int i = 0; i < indices.length; i++) {
+            found = false;
+            for (int j = 0; j < indices.length && !found; j++) {
+                if (i == indices[j]) {
+                    newS = newS + s.charAt(j);
+                    found = true;
+                }
+            }
+        }
+        return newS;
+    }
+}
