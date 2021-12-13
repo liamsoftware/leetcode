8a5389f962a737d71843e891d8dc3136dedb3344
LiamHayes1 <liamhayes@live.ie>
2021-12-13 10:30:11 +0000

    Build array from perm [easy]

---
+++ src/com/liamhayes/BuildArrayFromPermutation.java
@@ -0,0 +1,17 @@
+package com.liamhayes;
+
+public class BuildArrayFromPermutation {
+
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+        buildArray(nums);
+    }
+
+    public static int[] buildArray(int[] nums) {
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = nums[nums[i]];
+        }
+        return output;
+    }
+}
