b4d42058d31d6d78d9e1cb734a16ad7b3db29f38
LiamHayes1 <liamhayes@live.ie>
2022-03-30 14:08:31 +0100

    Update with alternative cleaner solution

--- src/com/liamhayes/ValidAnagram.java
+++ src/com/liamhayes/ValidAnagram.java
@@ -48,4 +48,22 @@ public class ValidAnagram {
         return true;
     }
      */
+    
+    public static boolean isValid2(String s, String t) {
+        if (s.length() != t.length()) return false;
+        
+        int[] counts = new int[26];
+        
+        for (int i = 0 ; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+        
+        for (int c : counts) {
+            if (c != 0) return false;
+        }
+        
+        return true;
+    }
+    
 }
