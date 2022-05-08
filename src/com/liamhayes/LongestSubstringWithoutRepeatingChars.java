9c45574a39d06214b72f23533e12903163d686ad
LiamHayes1 <liamhayes@live.ie>
2022-05-08 11:08:18 +0100

    3. Longest substring without repeating chars [m]

---
+++ src/com/liamhayes/LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,30 @@
+package com.liamhayes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingChars {
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, len = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start) {
+                    start = map.get(c) + 1;
+                }
+            }
+            len = Math.max(len, i - start + 1);
+            map.put(c, i);
+        }
+        return len;
+    }
+}
